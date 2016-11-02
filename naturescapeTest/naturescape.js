489e426eb362aab919a915be330f006a2769764f
J-Kilby <KilbyJA@gmail.com>
2016-11-02 12:02:36 +1100

    sh

--- naturescapeTest/naturescape.js
+++ naturescapeTest/naturescape.js
@@ -1,11 +1,14 @@
+window.onerror = function(message, url, lineNumber) {  
+    $("#5").html(message + " : " + lineNumber);
+    return true;
+};
+
 if (navigator != undefined){$("#1").html("navigator is defined");}
 else {$("#1").html("navigator is undefined")};
 if (navigator.geolocation != undefined){$("#2").html("navigator.geolocation is defined");}
 else {$("#2").html("navigator.geolocation is undefined")};
 if (navigator.geolocation.getCurrentPosition != undefined){$("#3").html("navigator.geolocation.getCurrentPosition is defined");}
 else {$("#3").html("navigator.geolocation.getCurrentPosition is undefined")};
-if (navigator.geolocation.getCurrentPosition != undefined){$("#4").html("navigator.geolocation.getCurrentPosition is defined");}
-else {$("#4").html("navigator.geolocation.getCurrentPosition is undefined")};
 
 navigator.geolocation.getCurrentPosition(function(position) {
     $("#4").html("fire!");
