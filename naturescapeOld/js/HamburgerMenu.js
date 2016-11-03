639d0cc494d56455498573e6bda3e4bb31267cbf
J-Kilby <KilbyJA@gmail.com>
2016-11-03 19:32:23 +1100

    Final Final

--- naturescapeOld/js/HamburgerMenu.js
+++
@@ -1,9 +0,0 @@
-// Hamburger Menu Show + Hide
-
-$(document).ready(function(){
-    $(".navicons > #menuicon").click(function(){
-        $(".nav > li").toggle(500);
-        $('#menuicon').toggleClass("selectedIcon");
-        $('#fbicon').toggle();
-    });
-});
